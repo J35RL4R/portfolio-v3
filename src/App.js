6b697cbc612c683a023d308196002de015617d72
Jesse R. Larsen <larsenj732@gmail.com>
2021-01-29 19:58:57 -0500

    about page render build out finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Nav from "./components/nav/nav";
+import Nav from "./components/nav/index";
+import Contentbox from "./components/contentbox/index"
+import Resumeblock from "./components/resumeblock/index"
 import './App.css';
 
 function App() {
   return (
     <Router>
       <Nav />
-  </Router>
+      <Contentbox>
+        <Resumeblock />
+      </Contentbox>
+    </Router>
   );
 }
 
