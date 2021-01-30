bbf86426b8bbdf1fb2d1bcbdddb40c9e53dafc91
Jesse R. Larsen <larsenj732@gmail.com>
2021-01-29 22:24:01 -0500

    functional portfolio page code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Nav from "./components/nav/index";
 import Contentbox from "./components/contentbox/index"
 import Resumeblock from "./components/resumeblock/index"
+import Portfolioblock from "./components/portfolioblock"
 import './App.css';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Nav />
       <Contentbox>
-        <Resumeblock />
+        <Portfolioblock/>
       </Contentbox>
     </Router>
   );
