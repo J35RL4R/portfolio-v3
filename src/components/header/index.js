ad21a05f51579a31a4834644bf9dcf78fd569146
Jesse R. Larsen <larsenj732@gmail.com>
2021-03-02 20:18:15 -0500

    converted navbar to react bootstrap.

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,39 +5,34 @@ import { Navbar, Nav } from "react-bootstrap";
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 function nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand">Jesse R. Larsen</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <div className="nav-item">
+    <Navbar bg="light" expand="lg">
+      <Navbar.Brand>Jesse R. Larsen</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse  id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Item>
+            <Nav.Link>
+              <Link
+                to="/portfolio-v3/"
+                className={
+                  window.location.pathname === "/portfolio-v3/"  ? "nav-link active" : "nav-link"}
+              >
+                About
+              </Link>
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Link>
             <Link
-            to="/portfolio-v3/"
-            className={
-            window.location.pathname === "/portfolio-v3/" || window.location.pathname === "/portfolio-v3/"
-              ? "nav-link active"
-              : "nav-link"
-          }
+              to="/portfolio"
+              className={
+                window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
             >
-            About
+              Portfolio
             </Link>
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link
-            to="/portfolio"
-            className={
-            window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link" }
-          >
-          Portfolio
-          </Link>
-        </li>
-      </ul>
-      </div>
-    </nav>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar >
   );
 }
 
