11bb2018077b770b33fccc019fb50e006fa6f985
Jesse R. Larsen <larsenj732@gmail.com>
2021-03-02 19:47:43 -0500

    adding react bootstrap

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 // Depending on the current path, this component sets the "active" className on the appropriate navigation link item
 function nav() {
